07cba3e1bb4649399b14fbb48da6d47942713f1a
MBAREK0 <elaadraouimbarek2023@gmail.com>
2024-03-11 23:08:39 +0000

    add refresh jwt token

--- evento-frontend/src/main.js
+++ evento-frontend/src/main.js
@@ -1,23 +1,27 @@
-// main.js
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import axios from 'axios';
 import router from './router';
 
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/index-category';
+const app = createApp(App);
 
-axios.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.response && error.response.status === 401) {
-            router.push('/sign-in');
-        }
-        return Promise.reject(error);
-    }
-);
 
-createApp(App)
-    .use(router)
-    .mount('#app');
+const refreshAccessToken = () => {
+    axios.post('http://127.0.0.1:8000/auth/refresh')
+        .then(response => {
+            const newToken = response.data.access_token;
+            localStorage.setItem('accessToken', newToken);
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + newToken;
+        })
+        .catch(error => {
+            console.error('Token refresh failed:', error);
+        });
+};
+
+
+setInterval(() => {
+    refreshAccessToken();
+}, 30 * 60 * 1000); 
+
+app.use(router).mount('#app');
